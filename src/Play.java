b3bb1ed1bbd0a8e5e44536ca71ac965ae3557796
Cesar Oliveira <cesargusto@gmail.com>
2017-03-11 14:40:42 -0300

    Função mostra

--- src/Play.java
+++ src/Play.java
@@ -1,8 +1,12 @@
 
 public class Play {
 	//ESTE É UM TESTE DO GIT
+	public static void mostra(){
+		System.out.println("\nESTE É UM TESTE DE FUNÇÃO");
+	}
 	public static void main(String[] args) {
 		System.out.println("Ola Mundo Cesar");
+		mostra();
 
 	}
 
